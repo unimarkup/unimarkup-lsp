22d8ca89a663c28abbb8c7501e476f241c8df35b
Nadir Fejzic <nadirfejzo@gmail.com>
2022-07-31 19:31:56 +0200

    feat: use async implementation

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,8 +3,8 @@ use std::error::Error;
 use lsp_types::Url;
 use serde::Serialize;
 
+mod asynchron;
 mod capabilities;
-mod doc_sync;
 mod semantic_tokens;
 mod sync;
 
@@ -14,6 +14,22 @@ struct RenderedContent {
     content: String,
 }
 
+struct Config {
+    run_async: bool,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self { run_async: true }
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error + Sync + Send>> {
-    sync::run()
+    let config = Config::default();
+
+    if config.run_async {
+        asynchron::run()
+    } else {
+        sync::run()
+    }
 }
