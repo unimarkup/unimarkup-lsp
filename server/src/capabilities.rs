8d5a7d8301970e98e1d19dccaaa2d875a05150c5
Nadir Fejzic <nadirfejzo@gmail.com>
2022-10-02 19:08:55 +0200

    refactor: simplify capabilities setup

--- server/src/capabilities.rs
+++ server/src/capabilities.rs
@@ -1,58 +1,61 @@
 use lsp_types::{
-    SemanticTokenModifier, SemanticTokenType, SemanticTokensFullOptions, SemanticTokensOptions,
+    SemanticTokenModifier, SemanticTokenType, SemanticTokensFullOptions, SemanticTokensLegend,
+    SemanticTokensOptions, SemanticTokensServerCapabilities,
 };
 use lsp_types::{ServerCapabilities, TextDocumentSyncCapability, TextDocumentSyncKind};
 
 pub fn get_capabilities() -> ServerCapabilities {
-    let mut server_capabilities = ServerCapabilities::default();
-
-    let text_sync = Some(TextDocumentSyncCapability::Kind(TextDocumentSyncKind::FULL));
-
-    server_capabilities.text_document_sync = text_sync;
-
-    let token_provider = lsp_types::SemanticTokensServerCapabilities::from(SemanticTokensOptions {
+    let semantic_tokens_provider = SemanticTokensServerCapabilities::from(SemanticTokensOptions {
         full: Some(SemanticTokensFullOptions::Bool(true)),
-        legend: lsp_types::SemanticTokensLegend {
-            token_types: vec![
-                SemanticTokenType::NAMESPACE,
-                SemanticTokenType::TYPE,
-                SemanticTokenType::CLASS,
-                SemanticTokenType::ENUM,
-                SemanticTokenType::INTERFACE,
-                SemanticTokenType::STRUCT,
-                SemanticTokenType::TYPE_PARAMETER,
-                SemanticTokenType::PARAMETER,
-                SemanticTokenType::VARIABLE,
-                SemanticTokenType::PROPERTY,
-                SemanticTokenType::ENUM_MEMBER,
-                SemanticTokenType::EVENT,
-                SemanticTokenType::FUNCTION,
-                SemanticTokenType::METHOD,
-                SemanticTokenType::MACRO,
-                SemanticTokenType::KEYWORD,
-                SemanticTokenType::MODIFIER,
-                SemanticTokenType::COMMENT,
-                SemanticTokenType::STRING,
-                SemanticTokenType::NUMBER,
-                SemanticTokenType::REGEXP,
-                SemanticTokenType::OPERATOR,
-            ],
-            token_modifiers: vec![
-                SemanticTokenModifier::DECLARATION,
-                SemanticTokenModifier::DEFINITION,
-                SemanticTokenModifier::READONLY,
-                SemanticTokenModifier::STATIC,
-                SemanticTokenModifier::DEPRECATED,
-                SemanticTokenModifier::ABSTRACT,
-                SemanticTokenModifier::ASYNC,
-                SemanticTokenModifier::MODIFICATION,
-                SemanticTokenModifier::DOCUMENTATION,
-                SemanticTokenModifier::DEFAULT_LIBRARY,
-            ],
-        },
+        legend: create_semantic_token_legend(),
         ..Default::default()
-    });
+    })
+    .into();
 
-    server_capabilities.semantic_tokens_provider = Some(token_provider);
-    server_capabilities
+    ServerCapabilities {
+        text_document_sync: Some(TextDocumentSyncCapability::Kind(TextDocumentSyncKind::FULL)),
+        semantic_tokens_provider,
+        ..Default::default()
+    }
+}
+
+fn create_semantic_token_legend() -> SemanticTokensLegend {
+    SemanticTokensLegend {
+        token_types: vec![
+            SemanticTokenType::NAMESPACE,
+            SemanticTokenType::TYPE,
+            SemanticTokenType::CLASS,
+            SemanticTokenType::ENUM,
+            SemanticTokenType::INTERFACE,
+            SemanticTokenType::STRUCT,
+            SemanticTokenType::TYPE_PARAMETER,
+            SemanticTokenType::PARAMETER,
+            SemanticTokenType::VARIABLE,
+            SemanticTokenType::PROPERTY,
+            SemanticTokenType::ENUM_MEMBER,
+            SemanticTokenType::EVENT,
+            SemanticTokenType::FUNCTION,
+            SemanticTokenType::METHOD,
+            SemanticTokenType::MACRO,
+            SemanticTokenType::KEYWORD,
+            SemanticTokenType::MODIFIER,
+            SemanticTokenType::COMMENT,
+            SemanticTokenType::STRING,
+            SemanticTokenType::NUMBER,
+            SemanticTokenType::REGEXP,
+            SemanticTokenType::OPERATOR,
+        ],
+        token_modifiers: vec![
+            SemanticTokenModifier::DECLARATION,
+            SemanticTokenModifier::DEFINITION,
+            SemanticTokenModifier::READONLY,
+            SemanticTokenModifier::STATIC,
+            SemanticTokenModifier::DEPRECATED,
+            SemanticTokenModifier::ABSTRACT,
+            SemanticTokenModifier::ASYNC,
+            SemanticTokenModifier::MODIFICATION,
+            SemanticTokenModifier::DOCUMENTATION,
+            SemanticTokenModifier::DEFAULT_LIBRARY,
+        ],
+    }
 }
